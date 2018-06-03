1032084b97bb21febc73590f21a09d096ab04f52
Natthaphon Phanit <nulladivides@protonmail.com>
2018-06-04 00:51:04 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-full-name :firstName="firstName" :lastName="lastName"></app-full-name>
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" class="form-control" id="email" v-model="email">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="text" class="form-control" id="password" v-model="password">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="storeToDb">Store in Database?</label>
+                        <input type="checkbox" class="form-dontrol" id="storeToDb" v-model="storeToDb">
+                    </div>
+                    <!-- Exercise 1 -->
+                    <!-- Create a Signup Form where you retrieve the following Information -->
+                    <!-- Full Name (First Name + Last Name) -->
+                    <!-- Mail -->
+                    <!-- Password -->
+                    <!-- Store Data? Yes/No -->
+
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button class="btn btn-primary" @click.prevent="submit">Submit!</button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{ firstName }} {{ lastName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ storeToDb ? 'Yes' : 'No' }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import FullName from './components/FullName.vue'
+
+    export default {
+        data() {
+            return {
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: '',
+                storeToDb: false,
+                isSubmitted: false
+            };
+        },
+        methods: {
+            submit() {
+                this.isSubmitted = true;
+            }
+        },
+        components: {
+            'app-full-name': FullName
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/FullName.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="form-group">
+        <label for="firstName">First Name</label>
+        <input type="text" class="form-control" id="firstName" :value="firstName" @change="updateFirstName">
+        <label for="lastName">Last Name</label>
+        <input type="text" class="form-control" id="lastName" :value="lastName" @change="updateLastName">
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['firstName', 'lastName'],
+        methods: {
+            updateFirstName() {
+                this.$emit('input', this.firstName);
+            },
+            updateLastName() {
+                this.$emit('input', this.lastName);
+            }
+        }
+    }
+</script>
+
